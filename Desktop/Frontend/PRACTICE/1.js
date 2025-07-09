16945a8ebaaa1d71e8823c35e1f3046b2ed2f6b2
SnehaTalekar26 <talekarsneha26@gmail.com>
2025-07-09 12:54:05 +0530

    Re-added both projects correctly

---
+++ Desktop/Frontend/PRACTICE/1.js
@@ -0,0 +1,10 @@
+function submit(){
+    alert('Form Submitted')
+    // console.log('submit')
+    let names=document.getElementById('nameId');
+}
+function clear(){
+    alert('Form Cleared')
+    // console.log('clear')
+    let clears=document.getElementById('phoneId');
+}
